23fc8c473ed9ba1c7691cb9c69400222bfb94027
Suthan83 <suthan83@gmail.com>
2022-01-04 20:57:50 -0500

    use dynamic data and pass it down component using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
     return(
         <div className="expense-item">
-            <div >Date</div>
+            <div >{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Gym Membership</h2>
-                <div className="expense-item__price">$39.99</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
             
         </div>

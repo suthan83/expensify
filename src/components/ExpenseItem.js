ccd90ff0b2c4cc128ee488614ec49dbbd54d7117
Suthan83 <suthan83@gmail.com>
2022-01-04 20:42:30 -0500

    improve ExpenseItem component and add styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return(
-        <div>Expense Item</div>
+        <div className="expense-item">
+            <div >Date</div>
+            <div className="expense-item__description">
+                <h2>Gym Membership</h2>
+                <div className="expense-item__price">$39.99</div>
+            </div>
+            
+        </div>
     )
 }
 

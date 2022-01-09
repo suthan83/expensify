100b15d3e77f56fefc43c69d83bf83dc4cb0bd84
Suthan83 <suthan83@gmail.com>
2022-01-08 21:28:38 -0500

    Dummy data is move into state and now user can add to expenses to the list

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -17,11 +17,12 @@ const Expenses = (props) => {
     return (
         <div>
             <Card className='expenses'>
-            <ExpensesFilter filteredYear={filterSelection} onFilterSelection={filterOptionHandler}/>
-                <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-                <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-                <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-                <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+                <ExpensesFilter filteredYear={filterSelection} onFilterSelection={filterOptionHandler}/>
+                {expenses.map( 
+                    expense => {
+                        return <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
+                    }
+                )}
             </Card>
         </div>
     );

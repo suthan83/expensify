236fd95bdc2c604436cdfcb272e54e7c968a637a
Suthan83 <suthan83@gmail.com>
2022-01-15 12:09:48 -0500

    clean-up console logs

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,11 +4,10 @@ import './Chart.css'
 
 const Chart = (props) => {
     const valuesArray = props.dataPoints.map(dataPoint => {
-        console.log(dataPoint.value)
         return dataPoint.value
     });
+    
     const maxValue = Math.max(...valuesArray);
-    console.log(maxValue);
 
     return (
         <div className='chart'>
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -6,7 +6,6 @@ const ChartBar = (props) => {
     //determine bar height
     if (props.maxValue > 0) {
         barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
-        console.log(barFillHeight);
     }
 
     return (
